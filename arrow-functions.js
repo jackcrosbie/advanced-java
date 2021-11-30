cbc0af9dc4e8bc035af2e72fe55e382f5abfdb87
jackcrosbie <jack.crosbie@mycit.ie>
2021-11-30 14:51:33 +0000

    advanced JS examples

--- arrow-functions.js
+++ arrow-functions.js
@@ -4,11 +4,42 @@
  */
 
 // Vanilla JavaScript Function
+// function addTwoNumbers(a, b) {
+//     // code block
+//     return a + b;
+// }
+
+// let sum = addTwoNumbers(3, 5); //set function equal to variable
+// console.log(sum) //call variable to console
 
 // Arrow Function With Parameters
+const addTwoNumbers = (a, b) => {
+    // code block
+    return a + b;
+}
+
+let sum = addTwoNumbers(3, 5); //set function equal to variable
+console.log(sum) //call variable to console
+
 
 // Single Line Arrow Function With Parameters
+// const addTwoNumbers2 = (a, b) => a + b; vaild with or without brackets after fat arrow
+const addTwoNumbers2 = (a, b) => (a + b);
+let sum2 = addTwoNumbers2(6, 4)
+console.log(sum2)
 
 // Implicit Returns
+const saySomething = message => console.log(message);
+saySomething('Hello There!')
+
+const sayHello = () => console.log('Hello');
+sayHello()
 
 // Returning Multiple Lines
+
+const returnMultipleLines = () => (
+    `<p>
+      This is a multiline string!
+    </p>`
+)
+console.log(returnMultipleLines());
